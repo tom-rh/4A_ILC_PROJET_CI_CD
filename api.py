7f50ee3fc7056832be2f13c81f002b4f9b5f0e1e
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2023-01-31 12:34:26 +0100

    Loading CSV

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
-from flask import Flask
-
+from flask import Flask, request
 from operator import attrgetter
+
+import csv
+import os
 
 app = Flask(__name__)
 
@@ -50,6 +52,8 @@ transactions = []
 def donnerPersonnes():
     return [personne.toJSON() for personne in personnes]
 
+
+
 @app.route("/E1/<int:id1>/<int:id2>/<date>/<int:somme>", methods=['GET', 'POST'])
 def ajouterTransaction(id1, id2, date, somme):
     transaction = Transaction(personnes[int(id1)], personnes[int(id2)], date, int(somme))
@@ -57,9 +61,13 @@ def ajouterTransaction(id1, id2, date, somme):
     transactions.sort(key=attrgetter('date'))
     return transaction.toJSON()
 
+
+
 @app.route("/E2", methods=['GET'])
-def afficherTransactions():
+def donnerTransactions():
     return [transaction.toJSON() for transaction in transactions]
+
+
 
 @app.route("/E3/<int:id>", methods=['GET'])
 def donnerTransactionsPersonne(id):
@@ -69,8 +77,40 @@ def donnerTransactionsPersonne(id):
             out.append(transaction.toJSON())
     return out
 
+
+
 @app.route("/E4/<int:id>", methods=['GET'])
 def donnerSoldePersonne(id):
     return { "solde": personnes[int(id)].solde }
 
-# @app.route("/E5")
+
+
+@app.route("/E5/personnes", methods=['POST'])
+def chargerPersonnes():
+    if request.files:
+        file = request.files['personnes']
+        filepath = os.path.join(app.config['FILE_UPLOADS'], file.filename)
+        file.save(filepath)
+
+        with open(filepath) as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                personnes.append(Personne(len(personnes), row[0], row[1], int(row[2])))
+    else:
+        print("Erreur")    
+    return donnerPersonnes()
+
+@app.route("/E5/transactions", methods=['POST'])
+def chargerTransactions():
+    if request.files:
+        file = request.files['transactions']
+        filepath = os.path.join(app.config['FILE_UPLOADS'], file.filename)
+        file.save(filepath)
+
+        with open(filepath) as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                ajouterTransaction(int(row[0]), int(row[1]), row[2], int(row[3]))
+    else:
+        print("Erreur")      
+    return donnerTransactions()
